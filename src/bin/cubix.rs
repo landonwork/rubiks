c149ca7c80ac55eab69eba611b36dcae7b4a5fa6
Landon Work <landon.r.work@gmail.com>
2024-05-30 20:58:55 -0400

    small changes

--- src/bin/cubix.rs
+++ src/bin/cubix.rs
@@ -7,19 +7,44 @@ use crossterm::{
 };
 use ratatui::{prelude::*, widgets::*};
 
-use rubiks::{view::{MovesList, DisplayCube}, cube::{CubePath, Move}, cubelet::Axis};
+use rubiks::{
+    cube::{CubePath, Move},
+    cubelet::Axis,
+    // store::Store,
+    view::{DisplayCube, MovesList}
+};
 
 #[derive(Default)]
 struct App {
-    cube: CubePath,
+    tabs: Vec<Tab>,
+    active_tab: usize,
+    // store: Option<Store>,
     // input: String,
 }
+
+#[derive(Default)]
+struct Tab {
+    cube: CubePath,
+    // simplified_cube: CubePath,
+}
+
+impl App {
+    fn active(&self) -> &Tab {
+        self.tabs.get(self.active_tab).unwrap()
+    }
+
+    fn active_mut(&mut self) -> &mut Tab {
+        self.tabs.get_mut(self.active_tab).unwrap()
+    }
+}
+
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     let mut app = App::default();
+    app.tabs.push(Tab::default());
 
     let mut should_quit = false;
     while !should_quit {
@@ -39,47 +64,53 @@ fn handle_events(app: &mut App) -> io::Result<bool> {
                 match key.code {
                     KeyCode::Esc => { return Ok(true); }
                     KeyCode::Char('q') => {
-                        app.cube.make_move(Move(3, 0, Axis::Y));
+                        app.active_mut().cube.make_move(Move(3, 0, Axis::Y));
                     }
                     KeyCode::Char('Q') => {
-                        app.cube.make_move(Move(0, 3, Axis::Y));
+                        app.active_mut().cube.make_move(Move(0, 3, Axis::Y));
                     }
                     KeyCode::Char('e') => {
-                        app.cube.make_move(Move(1, 0, Axis::Y));
+                        app.active_mut().cube.make_move(Move(1, 0, Axis::Y));
                     }
                     KeyCode::Char('E') => {
-                        app.cube.make_move(Move(0, 1, Axis::Y));
+                        app.active_mut().cube.make_move(Move(0, 1, Axis::Y));
                     }
                     KeyCode::Char('w') => {
-                        app.cube.make_move(Move(3, 0, Axis::X));
+                        app.active_mut().cube.make_move(Move(3, 0, Axis::X));
                     }
                     KeyCode::Char('W') => {
-                        app.cube.make_move(Move(0, 3, Axis::X));
+                        app.active_mut().cube.make_move(Move(0, 3, Axis::X));
                     }
                     KeyCode::Char('s') => {
-                        app.cube.make_move(Move(1, 0, Axis::X));
+                        app.active_mut().cube.make_move(Move(1, 0, Axis::X));
                     }
                     KeyCode::Char('S') => {
-                        app.cube.make_move(Move(0, 1, Axis::X));
+                        app.active_mut().cube.make_move(Move(0, 1, Axis::X));
                     }
                     KeyCode::Char('a') => {
-                        app.cube.make_move(Move(3, 0, Axis::Z));
+                        app.active_mut().cube.make_move(Move(3, 0, Axis::Z));
                     }
                     KeyCode::Char('A') => {
-                        app.cube.make_move(Move(0, 3, Axis::Z));
+                        app.active_mut().cube.make_move(Move(0, 3, Axis::Z));
                     }
                     KeyCode::Char('d') => {
-                        app.cube.make_move(Move(1, 0, Axis::Z));
+                        app.active_mut().cube.make_move(Move(1, 0, Axis::Z));
                     }
                     KeyCode::Char('D') => {
-                        app.cube.make_move(Move(0, 1, Axis::Z));
+                        app.active_mut().cube.make_move(Move(0, 1, Axis::Z));
                     }
+                    // undo
                     KeyCode::Char('u') => {
-                        app.cube.pop();
+                        app.active_mut().cube.pop();
                     }
+                    // reset
                     KeyCode::Char('R') => {
-                        app.cube = CubePath::default();
+                        app.active_mut().cube = CubePath::default();
                     }
+                    // new tab
+                    // KeyCode::Char('N') => {
+                    //     app.active_mut().cube = CubePath::default();
+                    // }
                     _ => { }
                 }
             }
@@ -90,19 +121,37 @@ fn handle_events(app: &mut App) -> io::Result<bool> {
 
 fn ui(frame: &mut Frame, app: &App) {
     let main_layout = Layout::new(
+        Direction::Vertical,
+        [Constraint::Max(1), Constraint::Fill(1)]
+    ).split(frame.size());
+
+    let big_layout = Layout::new(
         Direction::Horizontal,
         [Constraint::Length(350), Constraint::Min(40)]
-    )
-    .split(frame.size());
+    ).split(main_layout[1]);
+
+    let tabs_layout = Layout::new(
+        Direction::Horizontal,
+        app.tabs.iter().map(|_| Constraint::Max(21))
+    ).split(main_layout[0]);
+
     let inner_layout = Layout::new(
         Direction::Vertical,
-        [Constraint::Length(80), Constraint::Min(15)]
-    )
-    .split(main_layout[0]);
+        [Constraint::Length(7), Constraint::Min(15)]
+    ).split(big_layout[0]);
+
+    // Tabs
+    app.tabs.iter().zip(tabs_layout.iter()).for_each(|(tab, rect)| {
+        frame.render_widget(
+            Paragraph::new(format!("{}", tab.cube.last_cube()))
+                .block(Block::default().borders(Borders::LEFT | Borders::RIGHT)),
+            *rect
+        );
+    });
 
     // Cube viewer
     frame.render_widget(
-        Paragraph::new(format!("{}", DisplayCube(app.cube.cubes[app.cube.cubes.len()-1].clone())))
+        Paragraph::new(format!("{}", DisplayCube(app.active().cube.last_cube().clone())))
             .block(Block::default().title("Current state").borders(Borders::ALL)),
         inner_layout[0]
     );
@@ -116,9 +165,9 @@ fn ui(frame: &mut Frame, app: &App) {
 
     // Path
     frame.render_widget(
-        Paragraph::new(format!("{}", MovesList(&app.cube.moves))).block(
+        Paragraph::new(format!("{}", MovesList(&app.active().cube.moves))).block(
             Block::default().title("Path").borders(Borders::ALL)
         ),
-        main_layout[1]
+        big_layout[1]
     );
 }
