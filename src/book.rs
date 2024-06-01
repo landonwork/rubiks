9fbbfaf51443377bedeb0681875f4b53e54adc51
Landon Work <landon.r.work@gmail.com>
2024-05-31 23:08:19 -0400

    basic pack and unpack

--- src/book.rs
+++ src/book.rs
@@ -13,6 +13,8 @@ pub struct Book(pub Arc<Tree>);
 /// `bytes` is the slice to be packed and `packed_bits` is the number of bits that each byte should be
 /// packed into (between 1 and 7).
 fn pack(bytes: &[u8], packed_bits: usize) -> Vec<u8> {
+    debug_assert!(0 < packed_bits && packed_bits < 8);
+
     let new_bits = bytes.len() * packed_bits;
     let size = new_bits / 8 + (new_bits % 8 > 0) as usize;
     let mut new = vec![0; size];
@@ -22,7 +24,7 @@ fn pack(bytes: &[u8], packed_bits: usize) -> Vec<u8> {
         let end_bit = bit + packed_bits;
         let i = bit / 8;
         let bit_i = bit % 8;
-        if i == end_bit / 8 {
+        if i == (end_bit - 1) / 8 {
             new[i] |= byte << (8 - bit_i - packed_bits);
         } else {
             let end_bit_i = end_bit % 8;
@@ -44,6 +46,7 @@ fn unpack(bytes: &[u8], packed_bits: usize) -> Vec<u8> {
     debug_assert!(0 < packed_bits && packed_bits < 8);
 
     let size = bytes.len() * 8 / packed_bits;
+    println!("size: {size}");
     let mut new = vec![0; size];
 
     let mut bit = 0;
@@ -53,7 +56,7 @@ fn unpack(bytes: &[u8], packed_bits: usize) -> Vec<u8> {
         let bit_i = bit % 8;
 
         if i == (end_bit - 1) / 8 {
-            *byte = ((u8::MAX >> (8 - bit_i - packed_bits)) & bytes[i]) >> (8 - bit_i);
+            *byte = (u8::MAX >> (8 - packed_bits)) & (bytes[i] >> (8 - bit_i - packed_bits));
         } else {
             let end_bit_i = end_bit % 8;
             *byte = (((u8::MAX >> bit_i) & bytes[i]) << end_bit_i) | (bytes[i+1] >> (8 - end_bit_i));
@@ -77,4 +80,29 @@ mod tests {
         let actual = pack(bytes.as_slice(), 2);
         assert_eq!(expected.as_slice(), actual.as_slice());
     }
+
+    #[test]
+    fn test_pack_long() {
+        let bytes = [0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111];
+        let expected = [0b00000101, 0b00111001, 0b01110111];
+        let actual = pack(bytes.as_slice(), 3);
+        assert_eq!(expected.as_slice(), actual.as_slice());
+    }
+
+    #[test]
+    fn test_unpack() {
+        let bytes = [0b00011011];
+        let expected = [0b00, 0b01, 0b10, 0b11];
+        let actual = unpack(bytes.as_slice(), 2);
+        assert_eq!(expected.as_slice(), actual.as_slice());
+    }
+
+    #[test]
+    fn test_unpack_long() {
+        let bytes = [0b00000101, 0b00111001, 0b01110111];
+        let expected = [0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111];
+        let actual = unpack(bytes.as_slice(), 3);
+        assert_eq!(expected.as_slice(), actual.as_slice());
+    }
+
 }
