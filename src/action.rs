c910f18b72892ab2f7ecd03281728ede9c84a393
Landon Work <landon.r.work@gmail.com>
2024-06-01 20:00:58 -0400

    turns and quarter turns

--- src/action.rs
+++ src/action.rs
@@ -15,6 +15,14 @@ use crate::cubelet::Axis;
 #[derive(Clone, Copy, Debug, Hash)]
 pub struct Move(pub Axis, pub u8, pub u8);
 
+impl PartialEq for Move {
+    fn eq(&self, other: &Self) -> bool {
+        (self.1 == 0 && self.2 == 0 && other.1 == 0 && other.2 == 0)
+            || (self.0 == other.0 && self.1 == other.1 && self.2 == other.2)
+    }
+}
+impl Eq for Move {}
+
 impl Display for Move {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}{}{}", self.0, self.1, self.2)
@@ -89,3 +97,132 @@ impl Move {
     }
 }
 
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[repr(u8)]
+enum Turn {
+    L, L2, L3,
+    R, R2, R3,
+    U, U2, U3,
+    D, D2, D3,
+    F, F2, F3,
+    B, B2, B3,
+}
+
+impl From<Turn> for Move {
+    fn from(value: Turn) -> Self {
+        match value {
+            Turn::L => Move(Axis::X, 1, 0),
+            Turn::L2 => Move(Axis::X, 2, 0),
+            Turn::L3 => Move(Axis::X, 3, 0),
+            Turn::R => Move(Axis::X, 0, 3),
+            Turn::R2 => Move(Axis::X, 0, 2),
+            Turn::R3 => Move(Axis::X, 0, 1),
+            Turn::F => Move(Axis::Y, 1, 0),
+            Turn::F2 => Move(Axis::Y, 2, 0),
+            Turn::F3 => Move(Axis::Y, 3, 0),
+            Turn::B => Move(Axis::Y, 0, 3),
+            Turn::B2 => Move(Axis::Y, 0, 2),
+            Turn::B3 => Move(Axis::Y, 0, 1),
+            Turn::D => Move(Axis::Z, 1, 0),
+            Turn::D2 => Move(Axis::Z, 2, 0),
+            Turn::D3 => Move(Axis::Z, 3, 0),
+            Turn::U => Move(Axis::Z, 0, 3),
+            Turn::U2 => Move(Axis::Z, 0, 2),
+            Turn::U3 => Move(Axis::Z, 0, 1),
+        }
+    }
+}
+
+impl Display for Turn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::L => "L",
+                Self::L2 => "L2",
+                Self::L3 => "L'",
+                Self::R => "R",
+                Self::R2 => "R2",
+                Self::R3 => "R'",
+                Self::U => "U",
+                Self::U2 => "U2",
+                Self::U3 => "U'",
+                Self::D => "D",
+                Self::D2 => "D2",
+                Self::D3 => "D'",
+                Self::F => "F",
+                Self::F2 => "F2",
+                Self::F3 => "F'",
+                Self::B => "B",
+                Self::B2 => "B2",
+                Self::B3 => "B'",
+            }
+        )
+    }
+}
+
+impl Turn {
+    const ALL: [Self; 18] = [Self::L, Self::L2, Self::L3, Self::R, Self::R2, Self::R3, Self::U, Self::U2, Self::U3, Self::D, Self::D2, Self::D3, Self::F, Self::F2, Self::F3, Self::B, Self::B2, Self::B3];
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+#[repr(u8)]
+pub enum QuarterTurn {
+    L, L3,
+    R, R3,
+    U, U3,
+    D, D3,
+    F, F3,
+    B, B3,
+}
+
+impl From<QuarterTurn> for Move {
+    fn from(value: QuarterTurn) -> Self {
+        match value {
+            QuarterTurn::L => Move(Axis::X, 1, 0),
+            QuarterTurn::L3 => Move(Axis::X, 3, 0),
+            QuarterTurn::R => Move(Axis::X, 0, 3),
+            QuarterTurn::R3 => Move(Axis::X, 0, 1),
+            QuarterTurn::F => Move(Axis::Y, 1, 0),
+            QuarterTurn::F3 => Move(Axis::Y, 3, 0),
+            QuarterTurn::B => Move(Axis::Y, 0, 3),
+            QuarterTurn::B3 => Move(Axis::Y, 0, 1),
+            QuarterTurn::D => Move(Axis::Z, 1, 0),
+            QuarterTurn::D3 => Move(Axis::Z, 3, 0),
+            QuarterTurn::U => Move(Axis::Z, 0, 3),
+            QuarterTurn::U3 => Move(Axis::Z, 0, 1),
+        }
+    }
+}
+
+impl Display for QuarterTurn {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::L => "L",
+                Self::L3 => "L'",
+                Self::R => "R",
+                Self::R3 => "R'",
+                Self::U => "U",
+                Self::U3 => "U'",
+                Self::D => "D",
+                Self::D3 => "D'",
+                Self::F => "F",
+                Self::F3 => "F'",
+                Self::B => "B",
+                Self::B3 => "B'",
+            }
+        )
+    }
+}
+
+impl QuarterTurn {
+    const ALL: [Self; 12] = [Self::L, Self::L3, Self::R, Self::R3, Self::U, Self::U3, Self::D, Self::D3, Self::F, Self::F3, Self::B, Self::B3];
+}
+
+struct Word<T> {
+    seq: Vec<T>
+}
